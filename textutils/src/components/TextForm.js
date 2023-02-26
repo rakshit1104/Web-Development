a7518c260f53d265f5caa742f6c41eef9428b743
Rakshit Pandey <rakshit.lucky@gmail.com>
2023-02-26 14:21:27 +0530

    changes

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -11,14 +11,30 @@ export default function TextForm(prop) {
         let upperCase = text.toUpperCase();
         setText(upperCase);
     }
+
+    const LowerCaseOnClick = () => {
+        let lowerCase = text.toLowerCase();
+        setText(lowerCase);
+    }
     return (
-        <div>
-            <div className="mb-3">
-                <h1>{prop.heading}</h1>
-                <label htmlFor="myBox" className="form-label"></label>
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <>
+            <div>
+                <div className="container">
+                    <h1>{prop.heading}</h1>
+                    <label htmlFor="myBox" className="form-label"></label>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
+                <div className="container my-2">
+                <button className="btn btn-primary" onClick={upperCaseOnClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-1" onClick={LowerCaseOnClick}>Convert to Lowercase</button>
+                </div>
+                <div className="container">
+                    <h1>Your text summary</h1>
+                    <p>Characters: {text.length}</p>
+                    <p>Words: {text.split(" ").length}</p>
+                    <p>This can be read in: {0.008 * text.split(" ").length} minutes</p>
+                </div>
             </div>
-            <button className="btn btn-primary" onClick={upperCaseOnClick}>Convert to Uppercase</button>
-        </div>
+        </>
     )
 }
